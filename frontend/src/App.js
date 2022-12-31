e39c34e29c544179e183e1b025500859d73505c2
Ashish Kumar <98329891+aashishh121@users.noreply.github.com>
2022-12-31 13:58:45 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, {useEffect,useState} from 'react'
+import './App.css';
+
+import BookSeats from './Components/BookSeats';
+
+function App() {
+
+  const [seats, setSeats] = useState([]);
+
+  useEffect(() => {
+    getSeats();
+  }, []);
+
+  // this function is used to fetch the data of available seats from mongodb database.
+  const getSeats = async () => {
+    let result = await fetch("http://localhost:8000/seats");
+    result = await result.json();
+    setSeats(result);
+  }
+
+
+  return (
+    <div className="App">
+      <BookSeats valuee={seats} getSeats={getSeats} />
+    </div>
+  );
+}
+
+export default App;
